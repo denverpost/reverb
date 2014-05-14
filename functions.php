e22c94bfc9cb1087db82161e651895f65ab9dde1
schneidan <dan@scrollwright.com>
2014-05-14 11:13:23 -0600

    Initial commit.

---
+++ functions.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Reactor Child Theme Functions
+ *
+ * @package Reactor
+ * @author Anthony Wilhelm (@awshout / anthonywilhelm.com)
+ * @version 1.1.0
+ * @since 1.0.0
+ * @copyright Copyright (c) 2013, Anthony Wilhelm
+ * @license GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
+/* -------------------------------------------------------
+ You can add your custom functions below
+-------------------------------------------------------- */
+
+
+/**
+ * Child Theme Features
+ * The following function will allow you to remove features included with Reactor
+ *
+ * Remove the comment slashes (//) next to the functions
+ * For add_theme_support, remove values from arrays to disable parts of the feature
+ * remove_theme_support will disable the feature entirely
+ * Reference the functions.php file in Reactor for add_theme_support functions
+ */
+add_action('after_setup_theme', 'reactor_child_theme_setup', 11);
+
+
+function reactor_child_theme_setup() {
+
+    /* Support for menus */
+	// remove_theme_support('reactor-menus');
+	// add_theme_support(
+	// 	'reactor-menus',
+	// 	array('top-bar-l', 'top-bar-r', 'main-menu', 'side-menu', 'footer-links')
+	// );
+	
+	/* Support for sidebars
+	Note: this doesn't change layout options */
+	// remove_theme_support('reactor-sidebars');
+	// add_theme_support(
+	// 	'reactor-sidebars',
+	// 	array('primary', 'secondary', 'front-primary', 'front-secondary', 'footer')
+	// );
+	
+	/* Support for layouts
+	Note: this doesn't remove sidebars */
+	// remove_theme_support('reactor-layouts');
+	// add_theme_support(
+	// 	'reactor-layouts',
+	// 	array('1c', '2c-l', '2c-r', '3c-l', '3c-r', '3c-c')
+	// );
+	
+	/* Support for custom post types */
+	// remove_theme_support('reactor-post-types');
+	// add_theme_support(
+	// 	'reactor-post-types',
+	// 	array('slides', 'portfolio')
+	// );
+	
+	/* Support for page templates */
+	// remove_theme_support('reactor-page-templates');
+	// add_theme_support(
+	// 	'reactor-page-templates',
+	// 	array('front-page', 'news-page', 'portfolio', 'contact')
+	// );
+	
+	/* Remove support for background options in customizer */
+	// remove_theme_support('reactor-backgrounds');
+	
+	/* Remove support for font options in customizer */
+	// remove_theme_support('reactor-fonts');
+	
+	/* Remove support for custom login options in customizer */
+	// remove_theme_support('reactor-custom-login');
+	
+	/* Remove support for breadcrumbs function */
+	// remove_theme_support('reactor-breadcrumbs');
+	
+	/* Remove support for page links function */
+	// remove_theme_support('reactor-page-links');
+	
+	/* Remove support for page meta function */
+	// remove_theme_support('reactor-post-meta');
+	
+	/* Remove support for taxonomy subnav function */
+	// remove_theme_support('reactor-taxonomy-subnav');
+	
+	/* Remove support for shortcodes */
+	// remove_theme_support('reactor-shortcodes');
+	
+	/* Remove support for tumblog icons */
+	// remove_theme_support('reactor-tumblog-icons');
+	
+	/* Remove support for other langauges */
+	// remove_theme_support('reactor-translation');
+		
+}
+
+// add a favicon to the site
+function blog_favicon() {
+	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('stylesheet_directory').'/images/favicon.ico" />' . "\n";
+}
+add_action('wp_head', 'blog_favicon');
+
+// Hide the Wordpress admin bar for everyone
+function my_function_admin_bar(){ return false; }
+add_filter( 'show_admin_bar' , 'my_function_admin_bar');
+
+// Function to add featured image in RSS feeds
+function featured_image_in_rss($content)
+{
+    // Global $post variable
+    global $post;
+    // Check if the post has a featured image
+    if (has_post_thumbnail($post->ID))
+    {
+        $content = get_the_post_thumbnail($post->ID, 'full', array('style' => 'margin-bottom:10px;')) . $content;
+    }
+    return $content;
+}
+// Add the filter for RSS feeds Excerpt
+add_filter('the_excerpt_rss', 'featured_image_in_rss');
+//Add the filter for RSS feed content
+add_filter('the_content_feed', 'featured_image_in_rss');
+
+// Disable those annoying pingbacks from our own posts
+function disable_self_trackback( &$links ) {
+  foreach ( $links as $l => $link )
+        if ( 0 === strpos( $link, get_option( 'home' ) ) )
+            unset($links[$l]);
+}
+add_action( 'pre_ping', 'disable_self_trackback' );
+
+// Add contact methods fields to user profile
+function modify_contact_methods($profile_fields) {
+
+    // Add new fields
+    $profile_fields['publication'] = 'Publication';
+
+    return $profile_fields;
+}
+add_filter('user_contactmethods', 'modify_contact_methods');
+
+// Add Photographer Name and URL fields to media uploader
+function be_attachment_field_credit( $form_fields, $post ) {
+    $form_fields['be-photographer-name'] = array(
+        'label' => 'Photographer Name',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'be_photographer_name', true ),
+        'helps' => 'If provided, photo credit will be displayed',
+    );
+
+    $form_fields['be-photographer-org'] = array(
+        'label' => 'Photographer Organization',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'be_photographer_org', true ),
+        'helps' => 'Add Photographer Organization',
+    );
+
+    return $form_fields;
+}
+add_filter( 'attachment_fields_to_edit', 'be_attachment_field_credit', 10, 2 );
+
+// Save values of Photographer Name and URL in media uploader
+function be_attachment_field_credit_save( $post, $attachment ) {
+    if( isset( $attachment['be-photographer-name'] ) )
+        update_post_meta( $post['ID'], 'be_photographer_name', $attachment['be-photographer-name'] );
+
+    if( isset( $attachment['be-photographer-org'] ) )
+        update_post_meta( $post['ID'], 'be_photographer_org', $attachment['be-photographer-org'] );
+
+    return $post;
+}
+add_filter( 'attachment_fields_to_save', 'be_attachment_field_credit_save', 10, 2 );
+
+// Disables automatic Wordpress core updates:
+define( 'WP_AUTO_UPDATE_CORE', false );
+
+//This function intelligently trims a body of text to a certain number of words, but will not break a sentence.
+function smart_trim($instring, $truncation) {
+	//a little regex kills datelines
+    $instring = preg_replace("/\A((([A-Z ]+)\\,\s?([a-zA-Z ]+)\\.?)|[A-Z]+)\s?(&#8211;|&#8212;?)\s?/u", "", $instring);
+    //replace closing paragraph tags with a space to avoid collisions after punctuation
+    $instring = str_replace("</p>", " ", $instring);
+    //strip the HTML tags and then kill the entities
+    $string = html_entity_decode( strip_tags($instring), ENT_QUOTES, 'UTF-8');
+
+    $matches = preg_split("/\s+/", $string);
+    $count = count($matches);
+
+    if($count > $truncation) {
+        //Grab the last word; we need to determine if
+        //it is the end of the sentence or not
+        $last_word = strip_tags($matches[$truncation-1]);
+        $lw_count = strlen($last_word);
+
+        //The last word in our truncation has a sentence ender
+        if($last_word[$lw_count-1] == "." || $last_word[$lw_count-1] == "?" || $last_word[$lw_count-1] == "!") {
+            for($i=$truncation;$i<$count;$i++) {
+                unset($matches[$i]);
+            }
+
+        //The last word in our truncation doesn't have a sentence ender, find the next one
+        } else {
+            //Check each word following the last word until
+            //we determine a sentence's ending
+            $ending_found = FALSE;
+            for($i=($truncation);$i<$count;$i++) {
+                if($ending_found != TRUE) {
+                    $len = strlen(strip_tags($matches[$i]));
+                    if($matches[$i][$len-1] == "." || $matches[$i][$len-1] == "?" || $matches[$i][$len-1] == "!") {
+                        //Test to see if the next word starts with a capital
+                        if($matches[$i+1][0] == strtoupper($matches[$i+1][0])) {
+                            $ending_found = TRUE;
+                        }
+                    }
+                } else {
+                    unset($matches[$i]);
+                }
+            }
+        }
+        $body = implode(' ', $matches);
+        return $body;
+    } else {
+        return $string;
+    }
+}
+
+// Exclude Pages from search
+function SearchFilter($query) {
+    if ($query->is_search) {
+        $query->set('post_type', 'post');
+    }
+    return $query;
+}
+add_filter('pre_get_posts','SearchFilter');
+
+/**
+ * Include posts from authors in the search results where
+ * either their display name or user login matches the query string
+ *
+ * @author danielbachhuber
+ */
+add_filter( 'posts_search', 'db_filter_authors_search' );
+function db_filter_authors_search( $posts_search ) {
+
+    // Don't modify the query at all if we're not on the search template
+    // or if the LIKE is empty
+    if ( !is_search() || empty( $posts_search ) )
+        return $posts_search;
+
+    global $wpdb;
+    // Get all of the users of the blog and see if the search query matches either
+    // the display name or the user login
+    add_filter( 'pre_user_query', 'db_filter_user_query' );
+    $search = sanitize_text_field( get_query_var( 's' ) );
+    $args = array(
+        'count_total' => false,
+        'search' => sprintf( '*%s*', $search ),
+        'search_fields' => array(
+            'display_name',
+            'user_login',
+        ),
+        'fields' => 'ID',
+    );
+    $matching_users = get_users( $args );
+    remove_filter( 'pre_user_query', 'db_filter_user_query' );
+    // Don't modify the query if there aren't any matching users
+    if ( empty( $matching_users ) )
+        return $posts_search;
+    // Take a slightly different approach than core where we want all of the posts from these authors
+    $posts_search = str_replace( ')))', ")) OR ( {$wpdb->posts}.post_author IN (" . implode( ',', array_map( 'absint', $matching_users ) ) . ")))", $posts_search );
+    return $posts_search;
+}
+/**
+ * Modify get_users() to search display_name instead of user_nicename
+ */
+function db_filter_user_query( &$user_query ) {
+
+    if ( is_object( $user_query ) )
+        $user_query->query_where = str_replace( "user_nicename LIKE", "display_name LIKE", $user_query->query_where );
+    return $user_query;
+}
+
+?>
