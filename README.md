ba148f3cac2503df87e02aa2b9bb7a990c5f7377
schneidan <dan@scrollwright.com>
2014-07-08 18:22:05 -0600

    Lots of top-bar tweaks; rudimentary header meta setup

---
+++ README.md
@@ -0,0 +1,73 @@
+# Jamestown
+
+A child theme for Reactor designed for The Denver Post's HeyReverb.com music site, focusing on video and photos.
+
+## Requirements
+
+#### Themes
+
+[Reactor (Customized DP version)](http://extras.denverpost.com/media/wp/reactor.zip)
+
+#### Plugins
+
+* [Jetpack](https://wordpress.org/plugins/jetpack/)
+* [WP-Email](https://wordpress.org/plugins/wp-email/)
+* [WP SEO](https://wordpress.org/plugins/wordpress-seo/)
+
+## Setup
+
+#### Site settings
+
+1. Go to Settings -> General
+	* Set the Site Title to "Hey Reverb"
+
+#### Setting up the Front Page
+
+1. Create a blank page called "HeyReverb"
+	* Select the template "Front Page" from the templates at right
+	* Don't put any content in the page body
+2. Head to Settings -> Reading and set "Front page displays"  to "A static page"
+	* Select the HeyReverb page you just created under "Front page"
+	* While you're here, don't forget to set the RSS feeds to "Summary"
+
+#### Plugin settings
+
+1. Under E-Mail -> E-Mail options:
+	* Set "E-Mail Text Link Style" to Custom
+	* Set it to: `<a href="%EMAIL_URL%" rel="nofollow" title="%EMAIL_TEXT%" target="_blank">%1$s</a>`
+2. Be sure to connect Jetpack to Wordpress.com
+
+#### Theme Customizations
+
+Appearance -> Customize
+
+1. General
+	* Uncheck "Show Title & Tagline"
+2. Navigation
+	* "Top Bar Title" is not displayed (doesn't matter what it is)
+	* Check "Fixed Top Bar"
+	* Check "Contain Top Bar Width"
+3. Posts & Pages
+	* Set "Default Layout" to "One Column"
+	* Delete all text from "Read More Text" and leave blank
+	* Uncheck "Show Post Meta"
+	* Uncheck "Show Comment Link"
+	* Uncheck "Show Breadcrumbs"
+4. Fonts & Colors
+	* Set "Title Font" to "Yanone Kaffesatz"
+	* Set "Content Font" to "Vollkorn"
+	* Set link color to #EEEEEE
+5. Front Page
+	* Check "Exclude From Blog"
+	* Set "Post Columns" to "1 Column"
+	* Check "Link Post Titles"
+	* Set "Number of Posts" to the number of tiles you want plus 1 (for top article). Initially we're using 7.
+
+#### Menus
+
+1. You will need one menu of categories. Be sure to check the box to display it in the Top Bar Right position. Should be titled "Top bar" or "top bar"
+2. Your footer menu must be called "footer links"
+	* Should include a link to the home page (Pages -> View All -> HeyReverb)
+3. Your last link should be "Subscribe by Email"
+	* Be sure the link URL is just "#subscribe"
+
---
+++ header.php
@@ -0,0 +1,339 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * @package Reactor
+ * @subpackge Templates
+ * @since 1.0.0
+ */?><!DOCTYPE html>
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if ( IE 7 )&!( IEMobile )]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if ( IE 8 )&!( IEMobile )]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+<head profile="http://gmpg.org/xfn/11">
+<?php
+
+function convert_smart_quotes($string)  { 
+    $search = array('&lsquo;','&rsquo;','&ldquo;','&rdquo;');
+    $replace = array('&#039;','&#039;','&#034;','&#034;');
+    return str_replace($search, $replace, $string); 
+} 
+
+//Twitter Cards
+$twitter_thumbs = '';
+$temp_post = '';
+$temp_auth = '';
+$temp_gplus = '';
+$ogtype = 'blog';
+$twitter_desc   = '';
+if ( is_single() || is_page() ) {
+    $twitter_thumbs = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');
+    $temp_post = get_post($post->ID);
+    $temp_auth = get_the_author_meta('twitter', $post->post_author);
+    $temp_gplus = get_the_author_meta('googleplus', $post->post_author);
+    $ogtype = 'article';
+    $twitter_desc = strip_tags(get_the_excerpt());
+    $twitter_desc = convert_smart_quotes(htmlentities($twitter_desc, ENT_QUOTES, 'UTF-8'));
+}
+$twitter_url    = get_permalink();
+$twitter_title  = get_the_title();
+$twitter_thumb = ( ($twitter_thumbs != '') ? $twitter_thumbs[0] : get_stylesheet_directory_uri() . '/images/facebooklogo600.jpg' );
+$twitter_user_id = ( ($temp_post != '') ? $temp_post->post_author : '@RVRB' );
+$twitter_creator = ( ($temp_auth != '') ? '@' . $temp_auth : '@RVRB' );
+$google_authorship = ( ($temp_gplus != '') ? '<link rel="author" href="' . $temp_gplus . '" />' : '<link rel="publisher" href="http://plus.google.com/100931264054788579031" />' );
+echo $google_authorship;
+?>
+
+<meta name="twitter:card" value="summary" />
+<meta name="twitter:url" value="<?php echo $twitter_url; ?>" />
+<meta name="twitter:title" value="<?php echo $twitter_title; ?>" />
+<meta name="twitter:description" value="<?php echo $twitter_desc; ?>" />
+<meta name="twitter:image" value="<?php echo $twitter_thumb; ?>" />
+<meta name="twitter:site" value="@RVRB" />
+<meta name="twitter:domain" value="heyreverb.com" />
+<meta name="twitter:creator" content="<?php echo $twitter_creator; ?>" />
+
+<meta property="fb:app_id" content="589548971098932"/>
+<meta property="og:title" content="<?php if ( is_single() ) { wp_title(); } else { get_bloginfo('name'); } ?>" />
+<meta property="og:type" content="<?php echo $ogtype; ?>" />
+<meta property="og:url" content="<?php echo get_permalink() ?>" />
+<meta property="og:image" content="<?php echo $twitter_thumb; ?>" />
+<meta property="og:site_name" content="<?php bloginfo('name') ?>" />
+<meta property="og:description" content="<?php echo $twitter_desc; ?>" />
+<meta property="article:publisher" content="http://www.facebook.com/heyreverb" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php if ( !is_single() || !is_page() ) { ?>
+  <meta http-equiv="refresh" content="1800">
+<?php } ?>
+<meta name="distribution" content="global" />
+<?php echo ( (get_post_meta(get_the_ID(), 'sponsored_link', true) != '') ? '<meta name="Googlebot-News" content="noindex,follow">' : '' ); ?>
+<meta name="robots" content="follow, all" />
+<meta name="language" content="en, sv" />
+<meta name="Copyright" content="Copyright &copy; The Denver Post." />
+<meta name="news_keywords" content="colorado, reviews, <?php
+$GLOBALS['rel_art'] = '';
+if (has_tag() ) {
+    $posttags = get_the_tags();
+    foreach($posttags as $tag) {
+        $GLOBALS['rel_art'] .= ', ' . $tag->name;
+    }
+    echo $GLOBALS['rel_art'];
+    } ?>, music" />
+
+<!-- WordPress head -->
+<?php wp_head(); ?>
+<!-- end WordPress head -->
+<?php reactor_head(); ?>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet' type='text/css'>
+
+<?php $GLOBALS['dfmcat'] = '';
+  $GLOBALS['dfmby'] = '';
+  $GLOBALS['dfmid'] = '';
+  $GLOBALS['dfmthetitle'] = ( is_single() || is_page() ) ? html_entity_decode(get_the_title(), ENT_QUOTES) : wp_title('|', false, '');
+  $GLOBALS['dfmcat'] = array('','');
+if ( is_category() ) {
+    $category = get_the_category();
+    $GLOBALS['dfmcat'][0] = $category[0]->cat_name;
+} else if ( is_single() ) {
+    //$category = get_the_category();
+    $post_terms = wp_get_object_terms( $post->ID, 'category', array( 'fields' => 'ids' ) );
+    $separator = ', ';
+    $term_ids = implode( ',' , $post_terms );
+    $category = get_categories(array('hierarchical'=>true,'taxonomy'=>'category','order'=>'desc','number'=>1,'include'=>$term_ids));
+    $GLOBALS['dfmcat'][0] = ( ( $category[0]->category_parent != ( '' || null) ) ? get_cat_name($category[0]->category_parent) : $category[0]->cat_name );
+    $GLOBALS['dfmcat'][1] = ( ( $category[0]->category_parent != ( '' || null) ) ? $category[0]->cat_name : '');
+    $GLOBALS['dfmid'] = $post->ID;
+    $GLOBALS['dfmby'] = get_the_author_meta('display_name', $post->post_author);
+} else if ( is_tag() ) {
+    $GLOBALS['dfmcat'][0] = single_tag_title( '', false );
+} else if ( is_front_page() ) {
+    $GLOBALS['dfmcat'][0] = 'Home';
+} else if ( is_page() ) {
+    $GLOBALS['dfmcat'][0] = get_the_title();
+} ?>
+
+<script type="text/javascript">
+//Configure DFM Variables
+    dfm.api("data","siteId",          "Reverb"); //Assign a siteid for your property
+    dfm.api("data","siteName",        "Reverb"); //Assign user-friendly name for site
+    dfm.api("data","siteDomain",      "heyreverb.com"); //Domain of the News.com, WordPress, Blog
+    dfm.api("data","actualDomain",    "www.heyreverb.com");//Full URl of site
+    dfm.api("data","localCommonUrl",  "");
+    dfm.api("data","localAssetsUrl",  "");
+    dfm.api("data","contentId",       "<?php echo $GLOBALS['dfmid']; ?>");
+    dfm.api("data","sectionName",     "<?php echo $GLOBALS['dfmcat'][0]; ?>"); //Omniture section/category name
+    dfm.api("data","pageId",          "");
+    dfm.api("data","pageUrl",         "http://www.heyreverb.com/"); //Full URl of site
+    dfm.api("data","pageVanityUrl",   "http://www.heyreverb.com/"); //Full URl of site
+    dfm.api("data","pageTitle",       "<?php echo $GLOBALS['dfmthetitle']; ?>");
+    dfm.api("data","pageType",        "");
+    dfm.api("data","abstract",        "<?php echo $twitter_desc; ?>");
+    dfm.api("data","keywords",        "<?php echo substr($GLOBALS['rel_art'], 2); ?>");
+    dfm.api("data","title",           "<?php echo $GLOBALS['dfmthetitle']; ?>");
+    dfm.api("data","sectionId",       "<?php echo $GLOBALS['dfmcat'][1] != '' ? $GLOBALS['dfmcat'][1] : $GLOBALS['dfmcat'][0]; ?>");
+    dfm.api("data","slug",            "<?php echo ( is_single() ? $post->post_name : '' ); ?>");
+    dfm.api("data","byline",          "<?php echo $GLOBALS['dfmby']; ?>");
+    dfm.api("data","NetworkID",       "8013");
+    dfm.api("data","Taxonomy1",       "<?php echo $GLOBALS['dfmcat'][0]; ?>");
+    dfm.api("data","Taxonomy2",       "<?php echo $GLOBALS['dfmcat'][1] != '' ? $GLOBALS['dfmcat'][1] : ''; ?>");
+    dfm.api("data","Taxonomy3",       "");
+    dfm.api("data","Taxonomy4",       "");
+    dfm.api("data","kv",              "News");
+    dfm.api("data","omnitureAccount", "mngireverb"); //Omniture s_account.
+</script>
+
+<script type="text/javascript">
+    //configure Chartbeat variables
+    var _sf_startpt=(new Date()).getTime();
+    //confiure Outbrainvariables
+    var outbrainurl = '<?php echo get_permalink(); ?>';
+</script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-42541613-24', 'heyreverb.com');
+    ga('send', 'pageview');
+</script>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<div id="omniture">
+<!-- SiteCatalyst code version: H.17 Copyright 1997-2005 Omniture, Inc. More info available at http://www.omniture.com -->
+  <script language="JavaScript"><!--//
+          /* Specify the Report Suite ID(s) to track here */
+          var s_account= dfm.api("data","omnitureAccount");
+    //--></script>
+  <!-- Replaced SiteCatalystCode_H_22_1.js with SiteCatalystCode_H_22_1_NC.js -->
+  <script type="text/javascript" language="JavaScript" src='http://extras.mnginteractive.com/live/js/omniture/SiteCatalystCode_H_22_1_NC.js'></script>
+  <script type="text/javascript" language="JavaScript" src='http://extras.mnginteractive.com/live/js/omniture/OmniUserObjAndHelper.js'></script>
+  <script language="JavaScript"><!--
+
+          //Local Variables
+          var contentId = dfm.api("data","contentId");
+          var PaperBrand = getBrand2(s_account);
+          var PageName = dfm.api("data","pageTitle");
+          var SectionName = dfm.api("data","sectionName");
+
+      if (contentId != "" && contentId != null && SectionName == "") {
+        SectionName = dfm.api("data","sectionId");
+      }
+      var FriendlyName = SectionName;
+          var ArticleTitle = "";
+      if (contentId != "" && contentId != null) {
+        ArticleTitle = dfm.api("data","pageTitle");
+        FriendlyName = FriendlyName + " / "  +  ArticleTitle;
+      }
+          var domainName = getDomainName();
+          userObj = new omniObj();
+          userObj.load();
+          userObj.update();
+          userObj.save();
+          /* You may give each page an identifying name, server, and channel on the next lines. */
+          s.pageName=FriendlyName;
+          s.channel=SectionName; // Same as prop1
+          s.server="";// Blank
+          s.pageType=""; // Error pages ONLY
+          s.prop1="D=g";
+          beanprop2 = ("" != "") && ("" != null) ? "" : "?";
+          var escbeanprop2 = escape(beanprop2);
+          var unescbeanprop2 = unescape(escbeanprop2);
+          var articleId = dfm.api("data","contentId");        
+          beanprop3 = ("" != "") && ("" != null) ? "" : "?";
+          beanprop4 = ("" != "") && ("" != null) ? "" : "?";
+          if(articleId != "" && articleId != null){
+              beanprop5 = ("" != "") && ("" != null) ? "" : articleId +"_" +ArticleTitle;
+          } else {
+              beanprop5 = "?" ;
+          }
+                s.prop2='D=ch+' + "\"/" + unescbeanprop2 + "\"";
+                s.prop3='D=ch+' + "\"/" + unescbeanprop2 + "/" + beanprop3 + "\"";
+                s.prop4='D=ch+' + "\"/" + unescbeanprop2 + "/"+ beanprop3 + "/"+ beanprop4 + "\""; // Sub section 3  
+                s.prop5='D=ch+' + "\"/" + unescbeanprop2 + "/" + beanprop3 + "/" + beanprop4 + "/" + beanprop5 + "\""; // Sub section 4
+          s.prop6=dfm.api("data","Taxonomy1"); // Global - Section  TODO
+          s.prop7=dfm.api("data","Taxonomy2"); // Global - Sub section 1  TODO
+          s.prop8=dfm.api("data","Taxonomy3"); // Global - Sub section 2  TODO
+          var sourceVal = ""; // Source of request, i.e. RSS, flash, etc...
+          if(articleId != '' && articleId != null){
+              if(s.prop9 == '' || s.prop9 == null){
+                s.prop9 = sourceVal;
+              }
+          }
+          s.prop10=""; // Reserved for RSS
+          if(articleId != '' && articleId != null){
+              s.prop11=ArticleTitle=="null"?"":'D="'+domainName+' / "+v26+" / "+c50';
+          }
+          s.prop12=dfm.api("data","byline");
+          s.prop13=""; // Reserved for article
+          s.prop14=""; // Reserved for article
+          s.prop15=""; // Reserved for article
+          s.prop16=""; // Search
+          s.prop17=""; // Search
+          s.prop18=""; // Search
+          s.prop19=""; // Search
+          s.prop20=""; // Reserved for Search
+          s.prop21=""; // Reserved for Search
+          s.prop22=""; // Reserved for Search
+          s.prop23=""; // Reserved for Search
+          s.prop24=""; // Reserved for Search
+          s.prop25=""; // Reserved for Search
+          s.prop26=""; // 3rd Party Vendors
+          s.prop27=""; // OneSpot
+          s.prop28=""; // Blank
+          if(s.prop29 == null || s.prop29 == ''){
+              s.prop29="";
+          }
+          s.prop30=""; // Form Analysis Plugin
+          s.prop31=""; // Blank
+          s.prop32=""; // Blank
+          s.prop33=ArticleTitle=="null"?'D=c40+" / "+c43':'D=c40+" / "+c50+" / "+c9';
+          s.prop34=ArticleTitle=="null"?'D=c40+" / "+c43':'D=c40+" / "+c43+" / "+c50';
+          s.prop35=ArticleTitle=="null"?'D=v18=" / "+c40+" / "+c43':'D=v18+" / "+c40+" / "+c50';
+          s.prop36=isCampaign(getCampaignValue("EADID")+getCampaignValue("CREF"), PaperBrand, PageName, ArticleTitle); // Campaign Tracking Code  + Paper Brand + Page Name
+          s.prop37=isCampaign(getCampaignValue("IADID")+getCampaignValue("SOURCE"), PaperBrand, PageName, ArticleTitle); // Affiliate ID + Paper Brand + Page Name
+          s.prop38=isCampaign(getCampaignValue("PARTNERID"), PaperBrand, PageName,ArticleTitle); // Internal Referral ID + Paper Brand + Page Name
+          s.prop39="";                                                             // Search Engine + Keywords + Paper Brand + Page Name (populated by functions.js)
+          s.prop40=PaperBrand;                                                     // Paper Brand
+          s.prop41="";                                                             // Blank
+          s.prop42="";
+          s.prop43=SectionName; //
+          s.prop44=ArticleTitle=="null"?"":'D=c40+" / "+c43+" / "+v26';
+          s.prop45=ArticleTitle=="null"?"":'D=c40+" / "+c43+" / "+c12';
+          s.prop46=getArticleHelperPage(domainName,"22133898",location.href,ArticleTitle); // ArticleID + Special Page Name + Article Title
+          s.prop47=""; // Search
+          s.prop48=""; // Blank
+          s.prop49=""; // Blank
+          s.prop50=ArticleTitle;
+          /* E-commerce Variables */
+          s.campaign=getCiQueryString("EADID")+getCiQueryString("CREF");          //External Campaign - ?EADID=id
+          s.state="";
+          s.zip="";
+          s.events=getEvents(ArticleTitle, s.events);
+          s.products="";
+          s.purchaseID="";
+          s.eVar1=getCiQueryString("PARTNERID");                                  // Internal Campaign - ?PID=id
+          s.eVar2=getCiQueryString("IADID")+getCiQueryString("SOURCE");           // Affiliate ID - ?IADID=id
+          s.eVar3=getBrandOnChange(PaperBrand);                                             //Paper Brand
+          s.eVar4="D=pageName";
+          s.eVar5="";
+          s.eVar6="";
+          s.eVar7="";
+          s.eVar8="";
+          s.eVar9="";
+          s.eVar10="";
+          s.eVar11="";
+          s.eVar12="";
+          s.eVar13="";
+          s.eVar14=userObj.fPage|userObj.conPage|userObj.loginConPage?userObj.vType:''; //Visitory Type
+          s.eVar15=userObj.fPage|userObj.userIdChange?userObj.userId:'';                     // User ID
+          s.eVar16=userObj.conPage?userObj.rType:'';
+          s.eVar17="";
+          s.eVar18=getUserType();
+          s.eVar19=userObj.fPage|userObj.conPage|userObj.aaPage?userObj.regStatus:''; //Registration Status
+          s.eVar20=""; // Search
+          s.eVar21="";
+          s.eVar22="";
+          s.eVar23="";                                                      // Refinements
+          s.eVar24="D=c43";
+          s.eVar25="";
+          s.eVar26=articleId;  // article ID
+        s.eVar50=getCiQueryString("AADID");
+    //--></script>
+  <script type="text/javascript" src='http://extras.mnginteractive.com/live/js/omniture/functions.js'></script>
+  <script type="text/javascript"><!--//
+        var s_code=s.t();if(s_code)document.write(s_code);
+    //--></script>
+  <script language="JavaScript"><!--
+        if(navigator.appVersion.indexOf('MSIE')>=0)document.write(unescape('%3C')+'\!-'+'-');
+    //--></script>
+    <noscript><img src="http://NeBnr.112.2O7.net/b/ss/NeBnr/1/H.17--NS/0" height="0" width="0" border="0" alt="" style="margin:0;padding:0;" /></noscript>
+    <!-- End SiteCatalyst code version: H.17 -->
+</div>
+
+	<?php reactor_body_inside(); ?>
+
+    <div id="page" class="hfeed site"> 
+    
+        <?php reactor_header_before(); ?>
+    
+        <header id="header" class="site-header hide-for-small" role="banner">
+            <div class="row">
+                <div class="<?php reactor_columns( 12 ); ?>">
+                    
+                    <?php reactor_header_inside(); ?>
+                    
+                </div><!-- .columns -->
+            </div><!-- .row -->
+        </header><!-- #header -->
+        
+        <?php reactor_header_after(); ?>
+        
+        <div id="main" class="wrapper">
