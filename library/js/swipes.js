f744c126c3ad5416e1534b0f38ae8111339aa3dd
schneidan <dan@scrollwright.com>
2015-05-14 17:33:51 -0600

    Last tweak to try tonight

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function($) {
 		var hammerTime = new Hammer(hammerElem.get(0));
 		hammerTime.on("drag", function ( evnt ) {
 			var url = false;
+			console.log(evnt.direction);
 			if ( evnt.direction == 'right' ) {
 			    url = jQuery('.nav-next a').attr('href');
 			}		
