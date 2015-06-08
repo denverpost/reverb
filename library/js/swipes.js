a0d9951ffc449dfbdaaa411ab7444943502ec944
schneidan <dan@scrollwright.com>
2015-06-08 09:48:31 -0600

    Tweak scroll distance required for swipe-to-next

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     function hammerSwipe() {
     	var hammerElem = $('body.gesture');
 		var hammerTime = new Hammer(hammerElem.get(0));
-		hammerTime.get('pan').set({threshold:25});
+		hammerTime.get('pan').set({threshold:40});
 		var url = false;
 		hammerTime.on("panright", function ( evnt ) {
 			console.log(evnt.direction);
