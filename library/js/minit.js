a6f8519d3344b65f89019460c230092a8decf110
schneidan <dan@scrollwright.com>
2015-06-15 13:55:34 -0600

    actually fix $ is undefined

--- library/js/minit.js
+++ library/js/minit.js
@@ -1,6 +1,6 @@
-jQuery(document).on('mobileinit',function($){
-	$.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
-	$.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
-	$.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
-	$.event.special.swipe.durationThreshold = 1000;
+jQuery(document).on('mobileinit',function(){
+	jQuery.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
+	jQuery.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
+	jQuery.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
+	jQuery.event.special.swipe.durationThreshold = 1000;
 });
