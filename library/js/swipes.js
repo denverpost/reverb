797987f74f30bb7b46255887dec43d653f0f3a5b
schneidan <dan@scrollwright.com>
2015-06-15 13:59:25 -0600

    Replace only-mobile test

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -1,13 +1,15 @@
 jQuery(document).ready(function($) {
-	var next = $('.nav-next a').attr('href');
-    var prev = $('.nav-previous a').attr('href');
+	if ( $('.gesture').hasClass( 'iphone' ) ) {
+		var next = $('.nav-next a').attr('href');
+	    var prev = $('.nav-previous a').attr('href');
 
-	$(document).on('swipeleft swiperight', function(event) {
-		if ( event.type == 'swipeleft' ) {
-	        window.location = next;
-		} 
-		if ( event.type == 'swiperight' ) {
-	        window.location = prev;
-		}
-	});
-}); 
+		$(document).on('swipeleft swiperight', function(event) {
+			if ( event.type == 'swipeleft' ) {
+		        window.location = next;
+			} 
+			if ( event.type == 'swiperight' ) {
+		        window.location = prev;
+			}
+		});
+	}
+});
