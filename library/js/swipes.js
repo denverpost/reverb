7100e1338ddfc82812670bb8c9a252dfb35f6230
schneidan <dan@scrollwright.com>
2015-06-15 12:44:18 -0600

    Different way of triggering by swipe

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -27,27 +27,19 @@
 
 jQuery(document).ready(function($) {
 	console.log('got here');
+	$.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
+	$.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
+	$.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
+	$.event.special.swipe.durationThreshold = 1000;
+	next = $('.nav-next a').attr('href'), 
+    prev = $('.nav-previous a').attr('href');
 
-	$(document).on( "pageinit", ".gesture", function() {
-		$.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
-		$.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
-		$.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
-		$.event.special.swipe.durationThreshold = 1000;
-		var $page = $(this),
-		    page = "#" + $page.attr( "id" ), 
-		    next = $('.nav-next a').attr('href'), 
-		    prev = $('.nav-previous a').attr('href'); 
-
-		if ( next ) {  
-		    $page.on( "swipeleft", function() { 
-		        $.mobile.changePage( next , {transition: "slide"}); 
-		    }); 
+	$(document).on('swipeleft swiperight', function() {
+		if ( event.type == 'swipeleft' ) {
+	        $.mobile.changePage( next , {transition: "slide"});
 		} 
-
-		if ( prev ) { 
-		    $page.on( "swiperight", function() { 
-		        $.mobile.changePage( prev, { transition: "slide" , reverse: true} ); 
-		    }); 
+		if ( event.type == 'swiperight' ) {
+	        $.mobile.changePage( prev, { transition: "slide" , reverse: true} );
 		}
 	});
 }); 
