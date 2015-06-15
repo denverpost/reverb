1c24030b4f0da436ba8406d9784b429b6bf8a4c0
schneidan <dan@scrollwright.com>
2015-06-15 13:57:22 -0600

    Adjust threshold tolerances

--- library/js/minit.js
+++ library/js/minit.js
@@ -1,6 +1,6 @@
 jQuery(document).on('mobileinit',function(){
-	jQuery.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) / 60;
-	jQuery.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) / 60;
-	jQuery.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) / 13;
+	jQuery.event.special.swipe.scrollSupressionThreshold = (screen.availWidth) * .60;
+	jQuery.event.special.swipe.horizontalDistanceThreshold = (screen.availWidth) * .60;
+	jQuery.event.special.swipe.verticalDistanceThreshold = (screen.availHeight) * .10;
 	jQuery.event.special.swipe.durationThreshold = 1000;
 });
