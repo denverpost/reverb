ebc46b540140875420b319cd3e6bf67a2e9c35c0
schneidan <dan@scrollwright.com>
2015-06-16 15:14:59 -0600

    Rearrange and work on removing bar

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -1,47 +1,46 @@
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
-
-function bringTheBar(text,prev,next) {
-	text = typeof text !== 'undefined' ? text : 'Swipe to navigate!';
-	prev = prev ? '<span style="display:block;position:fixed;left:2%;bottom:7px;font-size:22px;font-weight:bold;">&lt;</span>' : '';
-	next = next ? '<span style="display:block;position:fixed;right:2%;bottom:7px;font-size:22px;font-weight:bold;">&gt;</span>' : '';
-	var fixedDiv = document.createElement('div');
-	fixedDiv.id = 'messageBar';
-	fixedDiv.style.position = 'fixed';
-	fixedDiv.style.bottom = '0';
-	fixedDiv.style.left = '0';
-	fixedDiv.style.height = '36px;';
-	fixedDiv.style.width = '100%';
-	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.5) 25%,rgba(0,0,0,1) 100%)';
-	fixedDiv.style.fontWeight = 'bold';
-	fixedDiv.style.color = '#FFFFFF';
-	fixedDiv.style.fontSize = '18px';
-	fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
-	fixedDiv.style.paddingBottom = '3px';
-	fixedDiv.style.textAlign = 'center';
-	fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:0;font-size:22px;font-weight:bold;">&lt;</span>' + text + '<span style="display:block;position:fixed;right:2%;bottom:0;font-size:22px;font-weight:bold;">&gt;</span>';
-	document.body.appendChild(fixedDiv);
-	var barTimeout = window.setTimeout(function(){
-		document.body.getElementById('messageBar').parentNode.removeChild('messageBar');
-	}),1500);
-}
-
 jQuery(document).ready(function($) {
+	function setCookie(cname, cvalue, exdays) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0; i<ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1);
+	        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+	    }
+	    return "";
+	}
+
+	function bringTheBar(text,prev,next) {
+		text = typeof text !== 'undefined' ? text : 'Swipe to navigate!';
+		prev = prev ? '<span style="display:block;position:fixed;left:2%;bottom:7px;font-size:22px;font-weight:bold;">&lt;</span>' : '';
+		next = next ? '<span style="display:block;position:fixed;right:2%;bottom:7px;font-size:22px;font-weight:bold;">&gt;</span>' : '';
+		var fixedDiv = document.createElement('div');
+		fixedDiv.id = 'messageBar';
+		fixedDiv.style.position = 'fixed';
+		fixedDiv.style.bottom = '0';
+		fixedDiv.style.left = '0';
+		fixedDiv.style.height = '36px;';
+		fixedDiv.style.width = '100%';
+		fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.5) 25%,rgba(0,0,0,1) 100%)';
+		fixedDiv.style.fontWeight = 'bold';
+		fixedDiv.style.color = '#FFFFFF';
+		fixedDiv.style.fontSize = '18px';
+		fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
+		fixedDiv.style.paddingBottom = '3px';
+		fixedDiv.style.textAlign = 'center';
+		fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:0;font-size:22px;font-weight:bold;">&lt;</span>' + text + '<span style="display:block;position:fixed;right:2%;bottom:0;font-size:22px;font-weight:bold;">&gt;</span>';
+		document.body.appendChild(fixedDiv);
+		window.setTimeout(function(){
+			$('#messageBar').remove();
+		}),1500);
+	}
 	if ( $('.gesture').hasClass( 'iphone' ) ) {
 		
 		var next = $('.nav-next a').attr('href');
