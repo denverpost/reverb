7d7bf8bfe43d3ae9bfa786a55ded6f17eef4adc7
schneidan <dan@scrollwright.com>
2015-06-16 12:10:55 -0600

    Testing checks...

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -1,5 +1,19 @@
+function bringTheBar(text) {
+	text = typeof text !== 'undefined' ? text : 'Swipe to navigate!';
+	var fixedDiv = document.createElement('div');
+	fixedDiv.id = 'message-slide-up';
+	fixedDiv.style.position = 'fixed';
+	fixedDiv.style.bottom = '0';
+	fixedDiv.style.left = '0';
+	fixedDiv.style.height = '60px;';
+	fixedDiv.style.width = '100%';
+	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(4,4,4,0.5) 23%,rgba(19,19,19,1) 100%);';
+	fixedDiv.style.color = '#FFFFFF';
+	fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
+}
+
 jQuery(document).ready(function($) {
-	if ( $('.gesture').hasClass( 'iphone' ) ) {
+	// if ( $('.gesture').hasClass( 'iphone' ) ) {
 		var next = $('.nav-next a').attr('href');
 	    var prev = $('.nav-previous a').attr('href');
 
@@ -11,5 +25,5 @@ jQuery(document).ready(function($) {
 		        window.location = prev;
 			}
 		});
-	}
+	// }
 });
