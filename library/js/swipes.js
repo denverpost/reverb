fc0904cbef1d1d6eecebe65894fe8924de82e9ca
schneidan <dan@scrollwright.com>
2015-06-16 14:34:30 -0600

    Working on the notification bar

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -5,11 +5,15 @@ function bringTheBar(text) {
 	fixedDiv.style.position = 'fixed';
 	fixedDiv.style.bottom = '0';
 	fixedDiv.style.left = '0';
-	fixedDiv.style.height = '60px;';
+	fixedDiv.style.height = '36px;';
 	fixedDiv.style.width = '100%';
 	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(4,4,4,0.5) 23%,rgba(19,19,19,1) 100%);';
 	fixedDiv.style.color = '#FFFFFF';
+	fixedDiv.style.fontSize = '18px';
 	fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
+	fixedDiv.style.textAlign = 'center';
+	fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:9px;">&lt;</span>' + text +'<span style="display:block;position:fixed;right:2%;bottom:9px;">&gt;</span>';
+	document.body.appendChild(fixedDiv);
 }
 
 jQuery(document).ready(function($) {
