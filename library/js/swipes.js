e2e0b19988f6527902f75f3248d8684ef01ab62b
schneidan <dan@scrollwright.com>
2015-06-16 15:00:37 -0600

    Bring the bar and set a cookie, or don't if there is a cookie

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -1,5 +1,25 @@
-function bringTheBar(text) {
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function bringTheBar(text,prev,next) {
 	text = typeof text !== 'undefined' ? text : 'Swipe to navigate!';
+	prev = prev ? '<span style="display:block;position:fixed;left:2%;bottom:7px;font-size:22px;font-weight:bold;">&lt;</span>' : '';
+	next = next ? '<span style="display:block;position:fixed;right:2%;bottom:7px;font-size:22px;font-weight:bold;">&gt;</span>' : '';
 	var fixedDiv = document.createElement('div');
 	fixedDiv.id = 'message-slide-up';
 	fixedDiv.style.position = 'fixed';
@@ -8,6 +28,7 @@ function bringTheBar(text) {
 	fixedDiv.style.height = '36px;';
 	fixedDiv.style.width = '100%';
 	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.5) 20%,rgba(0,0,0,1) 50%)';
+	fixedDiv.style.fontWeight = 'bold';
 	fixedDiv.style.color = '#FFFFFF';
 	fixedDiv.style.fontSize = '18px';
 	fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
@@ -18,18 +39,33 @@ function bringTheBar(text) {
 }
 
 jQuery(document).ready(function($) {
-	// if ( $('.gesture').hasClass( 'iphone' ) ) {
+	if ( $('.gesture').hasClass( 'iphone' ) ) {
+		
 		var next = $('.nav-next a').attr('href');
 	    var prev = $('.nav-previous a').attr('href');
-	    console.log('next: ' + next + ', prev: ' + prev);
+	    nextYes = next.indexOf('reverb') ? true : false;
+	    prevYes = prev.indexOf('reverb') ? true : false;
+
+		if (!getCookie('seenTheBar')) {
+			bringTheBar('Swipe to navigate!',prevYes,nextYes);
+			setCookie('seenTheBar',true,60);
+		}
 
 		$(document).on('swipeleft swiperight', function(event) {
-			if ( event.type == 'swipeleft' && next.indexOf('reverb') ) {
-		        window.location = next;
+			if ( event.type == 'swipeleft' ) {
+				if ( next.indexOf('reverb') ) {
+			        window.location = next;
+			    } else {
+			    	bringTheBar('Nothing newer posted.',true,false);
+			    }
 			} 
-			if ( event.type == 'swiperight' && prev.indexOf('reverb') ) {
-		        window.location = prev;
+			if ( event.type == 'swiperight' ) {
+		        if ( prev.indexOf('reverb') ) {
+		        	window.location = prev;
+		        } else {
+		        	bringTheBar('Nothing older posted.',false,true);
+		        }
 			}
 		});
-	// }
+	}
 });
