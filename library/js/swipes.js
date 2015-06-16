8c2ecd35deb170e9bb5bd2659d99a5db858e6228
schneidan <dan@scrollwright.com>
2015-06-16 16:04:03 -0600

    Disable remove for testing; tweak cookie to be site-wide

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function($) {
-	function setCookie(cname, cvalue, exdays) {
+	function setCookie(cname, cvalue, exdays, path, domain) {
 	    var d = new Date();
 	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	    var expires = "expires="+d.toUTCString();
+	    var expires = "expires="+d.toUTCString() +
+	    ((path == null) ? "; path=/" : "; path=" + path);
 	    document.cookie = cname + "=" + cvalue + "; " + expires;
 	}
 
@@ -34,14 +35,14 @@ jQuery(document).ready(function($) {
 		fixedDiv.style.fontSize = '18px';
 		fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
 		fixedDiv.style.paddingBottom = '3px';
-		fixedDiv.style.opacity = '0';
+		fixedDiv.style.opacity = '0.0';
 		fixedDiv.style.textAlign = 'center';
 		fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:0;font-size:22px;font-weight:bold;">&lt;</span>' + text + '<span style="display:block;position:fixed;right:2%;bottom:0;font-size:22px;font-weight:bold;">&gt;</span>';
 		document.body.appendChild(fixedDiv);
 		$('#messageBar').fadeIn('fast');
 		window.setTimeout(function(){
 			$('#messageBar').fadeOut('slow',function(){
-				$(this).remove();
+		//		$(this).remove();
 			});
 		},1500);
 	}
