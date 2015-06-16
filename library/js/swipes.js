74bb5774b7fdcf1baf5c3e457f220133a2858dc5
schneidan <dan@scrollwright.com>
2015-06-16 14:48:46 -0600

    Further bar tweaks

--- library/js/swipes.js
+++ library/js/swipes.js
@@ -7,12 +7,13 @@ function bringTheBar(text) {
 	fixedDiv.style.left = '0';
 	fixedDiv.style.height = '36px;';
 	fixedDiv.style.width = '100%';
-	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(4,4,4,0.5) 23%,rgba(19,19,19,1) 100%);';
+	fixedDiv.style.background = 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.5) 20%,rgba(0,0,0,1) 50%)';
 	fixedDiv.style.color = '#FFFFFF';
 	fixedDiv.style.fontSize = '18px';
 	fixedDiv.style.fontFamily = '\'Open Sans\',\'PT Sans\',Arial,Helvetica,sans-serif';
+	fixedDiv.style.paddingBottom = '3px';
 	fixedDiv.style.textAlign = 'center';
-	fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:9px;">&lt;</span>' + text +'<span style="display:block;position:fixed;right:2%;bottom:9px;">&gt;</span>';
+	fixedDiv.innerHTML = '<span style="display:block;position:fixed;left:2%;bottom:7px;font-size:22px;font-weight:bold;">&lt;</span>' + text + '<span style="display:block;position:fixed;right:2%;bottom:7px;font-size:22px;font-weight:bold;">&gt;</span>';
 	document.body.appendChild(fixedDiv);
 }
 
