e54aedab99801c5b71d937c746564b4c3753218f
Daniel J. Schneider <dan@scrollwright.com>
2016-08-24 13:15:44 -0600

    Work over the related template and styles to cute little cubes that will work great.

--- yarpp-template-rvrb.php
+++ yarpp-template-rvrb.php
@@ -19,14 +19,12 @@ Author: mitcho (Michael Yoshitaka Erlewine)
 			$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium' );
 		}
 		if ( isset( $large_image_url ) && strlen( $large_image_url[0] ) >= 1 ) { ?>
-			<div class="related-post" style="background-image:url( '<?php echo $large_image_url[0]; ?>' );">
-				<div class="front-thumbnail">
-					<div class="front-imgholder"></div>
-					<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-						<div class="front-img" style="background-image:url('<?php echo $large_image_url[0]; ?>');"></div>
-					</a>
-				</div>
-				<h2 class="entry-title">
+			<div class="related-post">
+				<div class="front-imgholder"></div>
+				<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+					<div class="front-img" style="background-image:url('<?php echo $large_image_url[0]; ?>');"></div>
+				</a>
+				<h2 <?php post_class('entry-title'); ?>>
 					<span>
 						<a href="<?php echo get_category_link( intval( $categories_list->term_id ) ); ?>"><?php echo $categories_list->cat_name; ?></a>
 					</span>
@@ -35,6 +33,8 @@ Author: mitcho (Michael Yoshitaka Erlewine)
 			</div>
 		<?php } ?>
 	<?php endwhile; ?>
+
+	<div class="clear"></div>
 
 <?php else: ?>
 
