8eaddd45d973b47f846030009203fa034eca175f
Daniel J. Schneider <dan@scrollwright.com>
2016-10-04 13:00:45 -0600

    JS work so expanding serach box works now. To animate or not to animate...

--- library/js/rv-functions.js
+++ library/js/rv-functions.js
@@ -8,8 +8,17 @@ function scrollDownTo(whereToScroll, scrollOffset) {
     return false;
 }
 
+function searchOpen() {
+  $j('#searchopen').on('click',function(){
+    $j(this).css('display','none');
+    $j('#searchformbox').removeClass('unopened');
+    $j('#searchformbox input#s').focus();
+  })
+}
+
 $j(document).ready(function() {
   $j(window).load(function() {
+    searchOpen();
     boxes = $j('.frontpage-widget.widget_dpe_fp_widget');
     maxHeight = Math.max.apply(
     Math, boxes.map(function() {
