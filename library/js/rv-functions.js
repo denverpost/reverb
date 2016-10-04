6a2cf4cb40e1fda06238319f089b1d739a563e38
Daniel J. Schneider <dan@scrollwright.com>
2016-10-04 13:49:31 -0600

    Add link so DP logo goes to homepage.

--- library/js/rv-functions.js
+++ library/js/rv-functions.js
@@ -8,16 +8,25 @@ function scrollDownTo(whereToScroll, scrollOffset) {
     return false;
 }
 
+function dpLogoClick() {
+  $j('#searchformbox.unopened').on('click',function(){
+    if ( $j(this).hasClass('unopened') ) {
+      window.location.href = 'http://www.denverpost.com';
+    }
+  });
+}
+
 function searchOpen() {
   $j('#searchopen').on('click',function(){
     $j(this).css('display','none');
     $j('#searchformbox').removeClass('unopened');
     $j('#searchformbox input#s').focus();
-  })
+  });
 }
 
 $j(document).ready(function() {
   $j(window).load(function() {
+    dpLogoClick();
     searchOpen();
     boxes = $j('.frontpage-widget.widget_dpe_fp_widget');
     maxHeight = Math.max.apply(
