5053c581302f672e7572b4facf5b19699406ec5a
Daniel J. Schneider <dan@scrollwright.com>
2016-10-22 17:48:44 -0600

    Add temporary image fix

--- functions.php
+++ functions.php
@@ -1186,3 +1186,20 @@ function tkno_admin_enqueue($hook) {
     wp_enqueue_script( 'rvadmin-js', get_stylesheet_directory_uri() . '/library/js/rv-admin.js' );
 }
 add_action( 'admin_enqueue_scripts', 'tkno_admin_enqueue' );
+
+
+/*
+Plugin Name: Default to GD
+Plugin URI: http://wordpress.org/extend/plugins/default-to-gd
+Description: Sets GD as default WP_Image_Editor class.
+Author: Mike Schroder
+Version: 1.0
+Author URI: http://www.getsource.net/
+*/
+function ms_image_editor_default_to_gd( $editors ) {
+    $gd_editor = 'WP_Image_Editor_GD';
+    $editors = array_diff( $editors, array( $gd_editor ) );
+    array_unshift( $editors, $gd_editor );
+    return $editors;
+}
+add_filter( 'wp_image_editors', 'ms_image_editor_default_to_gd' );
