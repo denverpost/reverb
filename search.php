f4be215d75fa763488e63c00c219757cbb9cfcee
Daniel J. Schneider <dan@scrollwright.com>
2016-10-27 16:54:03 -0600

    Better search numbers display

--- search.php
+++ search.php
@@ -22,18 +22,24 @@
                 
 			<?php if ( have_posts() ) :
 				$results_total = $wp_query->found_posts;
-				$results_first = 1 + ($wp_query->query_vars['posts_per_page'] * ( $paged - 1 ) );
-				if ( $wp_query->post_count == $wp_query->query_vars['posts_per_page'] ) {
-					$results_last = $wp_query->query_vars['posts_per_page'] * ( $paged );
+				if ( $paged == 0 ) {
+					$results_first = 1;
+					$results_last = $wp_query->query_vars['posts_per_page'];
 				} else {
-					$results_last = ( $wp_query->query_vars['posts_per_page'] * ( $paged - 1 ) ) + $wp_query->post_count;
+					$results_first = 1 + ($wp_query->query_vars['posts_per_page'] * ( $paged - 1 ) );
+					if ( $wp_query->post_count == $wp_query->query_vars['posts_per_page'] ) {
+						$results_last = $wp_query->query_vars['posts_per_page'] * ( $paged );
+					} else {
+						$results_last = ( $wp_query->query_vars['posts_per_page'] * ( $paged - 1 ) ) + $wp_query->post_count;
+					}
 				}
+				$entries = ( $wp_query->post_count == 1 ) ? 'entry' : 'entries';
 				?>
 			
 				<?php reactor_loop_before(); ?>
 				
                 	        <header class="page-header">
-                        	<h1 class="page-title"><span class="searchresults">Showing entries <strong><?php echo $results_first; ?></strong> to <strong><?php echo $results_last; ?></strong> (of <?php echo $results_total; ?>) for</span> <?php echo get_search_query(); ?></h1>
+                        	<h1 class="page-title"><span class="searchresults">Showing <?php echo $entries; ?> <strong><?php echo $results_first; ?></strong> <?php if ( $wp_query->post_count != 1 && $paged != 0 ): ?> to <strong><?php echo $results_last; ?></strong> <?php endif; ?>(of <?php echo $results_total; ?>) for</span> <?php echo get_search_query(); ?></h1>
                     		</header> 
 
 				<?php // start the loop
