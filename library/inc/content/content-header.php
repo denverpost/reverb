83b364a0bb5e716045651d7a0515e21def06c997
Daniel J. Schneider <dan@schneidan.com>
2017-11-02 16:30:38 -0600

    Add a customized FB share image for Outdoors non-article pages

--- library/inc/content/content-header.php
+++ library/inc/content/content-header.php
@@ -84,10 +84,12 @@ if ( is_home() || is_front_page() ) {
     }
     $GLOBALS['dfmby'] = get_the_author_meta('display_name', $post->post_author);
 }
-$twitter_thumb = ( ($twitter_thumbs != '') ? $twitter_thumbs[0] : get_stylesheet_directory_uri() . '/images/facebooklogo600.jpg' );
+$facebook_image = ( is_outdoors() ) ? '/images/facebook-share-outdoors.jpg' : '/images/facebooklogo600.jpg';
+$twitter_thumb = ( ($twitter_thumbs != '') ? $twitter_thumbs[0] : get_stylesheet_directory_uri() . $facebook_image );
 $twitter_user_id = ( ($temp_post != '') && is_single() ) ? $temp_post->post_author : '@thknwco';
 $twitter_creator = ( ($temp_auth != '') && is_single() ) ? '@' . $temp_auth : '@thknwco';
 echo ( ($temp_gplus != '') && is_single() ) ? '<link rel="author" href="' . $temp_gplus . '" />' : '<link rel="publisher" href="http://plus.google.com/100931264054788579031" />';
+echo ( is_outdoors() && ! is_single() ) ? "\n" . '<meta property="og:image" content="' . $twitter_thumb . '" />' : '';
 ?>
 
 <meta property="fb:pages" content="113250288696719">
