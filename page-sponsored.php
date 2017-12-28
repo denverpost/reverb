e86a76d986206f2d462a1a719330aa0136db4887
Daniel J. Schneider <dan@schneidan.com>
2017-12-28 13:20:14 -0700

    Empty #content div on sponsored page

--- page-sponsored.php
+++ page-sponsored.php
@@ -15,20 +15,7 @@
 	<div id="primary" class="site-content full-width">
     
         <div id="content" role="main">
-        	<div class="row">
-                <div class="<?php reactor_columns(12,12,12); ?>">
-                                
-                    <div id="nativo_author"><!-- @Author --></div>
-					<div id="nativo_authorlogo"><!-- @Authorlogo --></div>
-					<div id="nativo_title"><!-- @Title --></div>
-					<div id="nativo_headline"><!-- @Headline --></div>
-					<div id="nativo_content"><!-- @Content --></div>
-					<!-- Right Rail Container -->
-					<div id="rr-width-container"></div>
-
-                </div><!-- .columns -->
-                
-            </div><!-- .row -->
+        	
         </div><!-- #content -->
         
 	</div><!-- #primary -->
