b0704a8bfe8ef3c2b4a25bb0486ba54c402064da
Daniel J. Schneider <dan@schneidan.com>
2018-01-03 11:03:02 -0700

    Updated sponsored template

--- page-sponsored.php
+++ page-sponsored.php
@@ -15,21 +15,24 @@
 	<div id="primary" class="site-content full-width">
     
         <div id="content" role="main">
-        	<div class="row">
-                <div class="<?php reactor_columns(12,12,12); ?>">
-                                
-                    <div id="nativo_author"><!-- @Author --></div>
-					<div id="nativo_authorlogo"><!-- @Authorlogo --></div>
-					<div id="nativo_title"><!-- @Title --></div>
-					<div id="nativo_headline"><!-- @Headline --></div>
-					<div id="nativo_content"><!-- @Content --></div>
-					<!-- Right Rail Container -->
-					<div id="rr-width-container"></div>
-
-                </div><!-- .columns -->
-                
-            </div><!-- .row -->
-        </div><!-- #content -->
+		    <div class="row">
+		        <div class="large-8 columns 2c-l">
+		            <article class="post type-post status-publish format-standard has-post-thumbnail hentry single">
+		                <div class="entry-body">
+		                    <header class="entry-header">
+		                        <h1 class="entry-title"><!-- @Title --></h1>
+		                    </header><!-- .entry-header -->
+		                    <div class="entry-content">
+		                        <!-- @Content -->
+		                    </div><!-- .entry-content -->
+		                </div><!-- .entry-body -->
+		            </article>
+		        </div><!-- .columns -->
+		        <div id="sidebar" class="sidebar large-4 medium-6 small-12 columns" role="complementary">
+		        </div><!-- #sidebar -->
+		    </div>
+		    <!-- .row -->
+		</div>
         
 	</div><!-- #primary -->
 
