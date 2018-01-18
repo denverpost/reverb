2322639d5c0bb110b6327afc431dfb064c32092f
Daniel J. Schneider <dan@schneidan.com>
2018-01-18 15:55:19 -0700

    Add Listrak to footer, update ads.txt

--- footer.php
+++ footer.php
@@ -25,5 +25,33 @@
 
 <?php wp_footer(); reactor_foot(); ?>
 
+<script type='text/javascript'>
+/* <![CDATA[ */
+var biJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+(function(d, s, id, tid, vid) {
+    var js, ljs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = biJsHost + "cdn.listrakbi.com/scripts/script.js?m=" + tid + "&v=" + vid;
+    ljs.parentNode.insertBefore(js, ljs);
+})(document, 'script', 'ltkSDK', 'tUxHTINGb1zW', '1');
+(function(d) {
+    if (document.addEventListener) document.addEventListener('ltkAsyncListener', d);
+    else {
+        e = document.documentElement;
+        e.ltkAsyncProperty = 0;
+        e.attachEvent('onpropertychange', function(e) {
+            if (e.propertyName == 'ltkAsyncProperty') { d(); }
+        });
+    }
+})(function() {
+    /********** Begin Custom Code **********/
+    _ltk.Activity.AddPageBrowse();
+    _ltk.Activity.Submit();
+    /********** End Custom Code **********/
+});
+/* ]]> */
+</script>
 	</body>
 </html>
