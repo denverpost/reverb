5d7323493b63d5b4ecab2c22a7a3b8fd50a0cfdb
Chris Brubaker <cbrubaker@denverpost.com>
2019-02-12 12:52:08 -0700

    change to outdoors season landing pages
    Changed the display of content to be more in line with the entertainment side. I left the category-outdoorchild-orginal.phph file for future reference.

---
+++ category-outdoorchild-original.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: Outdoors Page
+ *
+ * @package Reactor
+ * @subpackage Page-Templates
+ * @since 1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary" class="site-content">
+    
+    	<?php reactor_content_before(); ?>
+  
+        <div id="content" role="main">
+        	<div class="row">
+
+                <div class="large-8 medium-12 small-12 columns" id="outdoorsmain">
+
+                    <?php
+                    $query_cat = $wp_query->get_queried_object();
+                    $class_header_category = ' category-' . tkno_get_top_category_slug( true, $query_cat->cat_id );
+                    $class_category = 'archive-title  category-' . $query_cat->slug; ?>
+                    <header class="archive-header<?php echo $class_header_category; ?>">
+                        <h1 class="<?php echo $class_category; ?>""><a href="javascript:void(0);" class="noclick"><?php echo $query_cat->name; ?></a></h1>
+                        <?php get_sidebar('categorysponsor'); ?>
+                    </header><!-- .archive-header -->
+
+                    <div class="article_wrapper_top">
+                        <?php get_template_part('loops/loop', 'outdoortop'); ?>
+                    </div>
+
+                    <div class="large-12 medium-6 small-12" id="outdoor-upper">
+
+                        <?php get_sidebar('outdoorupper'); ?>
+
+                    </div><!-- .columns -->
+                
+                    <?php reactor_inner_content_before(); ?>
+                
+                    <div class="article_wrapper">
+                        <?php get_template_part('loops/loop', 'outdoors'); ?>
+                    </div>
+
+                    <div class="clear"></div>
+                        
+                    <?php reactor_inner_content_after(); ?>
+
+                    <div class="large-12 medium-6 small-12" id="sidebar-outdoormiddle">
+
+                        <?php tkno_outdoor_children_below(); ?>
+
+                    </div><!-- .columns -->
+
+                </div><!-- .columns -->
+
+				<?php get_sidebar('outdoors'); ?>
+                
+            </div><!-- .row -->
+        </div><!-- #content -->
+        
+        <?php reactor_content_after(); ?>
+        
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
